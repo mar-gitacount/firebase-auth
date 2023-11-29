200615a45f24ac2f67e507f56cb4cb51c9aa7fc5
mar-gitacount <ichikawa.contact@gmail.com>
2023-11-30 08:41:07 +0900

    firebase設定完了

--- src/utils/init-firebase.ts
+++ src/utils/init-firebase.ts
@@ -3,12 +3,12 @@ import { getAuth, browserSessionPersistence } from 'firebase/auth';
 
 // FirebaseApp初期化
 initializeApp({
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.RREACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 console.log(process.env.REACT_APP_AUTH_PERSISIT);
