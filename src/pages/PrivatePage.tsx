d71f85201713440c2785348582a1614d9f2ea974
mar-gitacount <ichikawa.contact@gmail.com>
2023-12-06 00:18:54 +0900

    カレンダー機能編集　

--- src/pages/PrivatePage.tsx
+++ src/pages/PrivatePage.tsx
@@ -1,19 +1,62 @@
-import { VFC } from 'react';
+import { VFC, useState, useEffect } from 'react';
 import useAuthState from 'hooks/useAuthState';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 import FullCalendar from '@fullcalendar/react';
-import timeGridPlugin from '@fullcalendar/timegrid'; // 追加
+import  DateClickArg  from '@fullcalendar/react'; // ここでDateClickArgをインポート
+import timeGridPlugin from '@fullcalendar/timegrid';
 import dayGridPlugin from '@fullcalendar/daygrid';
-import jaLocale from '@fullcalendar/core/locales/ja'; 
-/**
- * 表示にログインを必要とするページ
- */
+import jaLocale from '@fullcalendar/core/locales/ja';
+
+// イベントデータの型
+interface EventData {
+  title: string;
+  start: Date;
+  end: Date;
+}
+
 const PrivatePage: VFC = () => {
-  const { isLoading, email, userName} = useAuthState();
-  console.log(useAuthState().userName);
-  // const name = useAuthState().userName
+  const { isLoading, email, userName, userId } = useAuthState();
+  const [events, setEvents] = useState<EventData[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const db = getFirestore();
+        const eventsRef = collection(db, `users/${userId}/events`);
+        const querySnapshot = await getDocs(eventsRef);
+
+        const eventData: EventData[] = [];
+
+        querySnapshot.forEach((doc) => {
+          const event: EventData = {
+            title: doc.data().title,
+            start: doc.data().start.toDate(),
+            end: doc.data().end.toDate(),
+          };
+          eventData.push(event);
+        });
+
+        setEvents(eventData);
+      } catch (error) {
+        console.error('Error fetching data: ', error);
+      }
+    };
+
+    fetchData();
+  }, [userId]);
+
+  const handleDateClick = (clickInfo: DateClickArg) => {
+    // クリックされた位置の日時情報を取得してコンソールに出力
+    console.log('Clicked date: ', clickInfo.dateStr);
+  };
+
+  const handleEventClick = (clickInfo: any) => {
+    console.log('Clicked event:', clickInfo);
+  };
+
   if (isLoading) {
-    return <p>Loadiing...</p>;
+    return <p>Loading...</p>;
   }
 
   return (
@@ -22,22 +65,21 @@ const PrivatePage: VFC = () => {
       <div>ようこそ{userName}さん!!</div>
       <div>{email}</div>
       <div>
-      <FullCalendar 
-      plugins={[dayGridPlugin,timeGridPlugin]} initialView="dayGridMonth"
-      locales={[jaLocale]}         
-      locale='ja'                 
-      headerToolbar={{                         
-        left: 'prev,next today',
-        center: 'title',
-        right: 'dayGridMonth,timeGridWeek',
-      }}
-      events={[
-        {title:'eventを', start: '2022-03-14'},
-        {title:'こんな感じで追加できます', start: '2022-03-15', end: '2022-03-17'}
-      ]}
-      />
-    </div>
-      
+        <FullCalendar
+          plugins={[dayGridPlugin, timeGridPlugin]}
+          initialView="timeGridDay"
+          locales={[jaLocale]}
+          locale='ja'
+          headerToolbar={{
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay',
+          }}
+          events={events}
+          eventClick={(clickInfo: any) => handleEventClick(clickInfo)}
+          dateClick={handleDateClick}
+        />
+      </div>
     </>
   );
 };
