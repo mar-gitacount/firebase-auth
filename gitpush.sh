8cfd737a86747c67ff50183d9d59c9a60ee696ca
mar-gitacont <ichikawa.contact@gmail.com>
2023-12-19 00:16:11 +0900

    シェルスクリプトからのコミット→ãプッシュ

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,13 @@
-echo "何か入力してください:"
+echo "git commitする際のコメントを記載してください。:"
 read userInput
+
 git add .
+echo  "git addしました。"
+
 # 入力された値を表示する
 echo "入力された内容は: $userInput です"
 
+git commit -m "$userInput"
+
+git push origin master
 
