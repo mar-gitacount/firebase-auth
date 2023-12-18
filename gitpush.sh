b65ced7e8261b1ae57770119b6c0d7276a4a0146
mar-gitacont <ichikawa.contact@gmail.com>
2023-12-19 00:31:53 +0900

    シェルスクリプト実行する。

--- gitpush.sh
+++ gitpush.sh
@@ -1,13 +1,26 @@
-echo "git commitする際のコメントを記載してください。:"
+#!/bin/bash
+
+# git branch の出力を変数に格納する
+branches=$(git branch)
+
+echo "現在$branches ブランチです。 続行しますか？ (y/n): "
 read userInput
-
-git add .
-echo  "git addしました。"
-
-# 入力された値を表示する
-echo "入力された内容は: $userInput です"
-
-git commit -m "$userInput"
-
-git push origin master
-
+if [ "$userInput" = "y" ]; then
+    echo "続行します"
+    #git add する。
+    git add .
+    echo "git addしました。"
+    echo "git commitする際のコメントを記載してください。:"
+    read commitMessage
+    # 入力された値を表示する
+    echo "入力された内容は: $commitMessage です"
+    git commit -m "$commitMessage"
+    git push origin master
+    # 続行する処理をここに記述します
+elif [ "$userInput" = "n" ]; then
+    echo "キャンセルします"
+    # キャンセルする処理をここに記述します
+else
+    echo "無効な入力です"
+    # その他の入力に対する処理をここに記述します
+fi
