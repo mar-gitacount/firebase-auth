f978e8f04e539905502da326b1dfca258e02c870
mar-gitacont <ichikawa.contact@gmail.com>
2023-12-23 19:33:25 +0900

    パッケージロックはなし

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { VFC } from 'react';
-import { Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import RequireAuth from './components/RequireAuth';
 import HomePage from './pages/HomePage';
 import PublicPage from './pages/PublicPage';
@@ -8,35 +8,28 @@ import LoginPage from './pages/LoginPage';
 import './App.css';
 import SignIn from 'pages/SignIn';
 
-/**
- * ルート定義
- * / トップページ。ログインしていると「ログアウト」ボタンを表示する
- * /public ログイン不要ページ
- * /private ログインが必要なページ(未ログインの場合、ログイン画面が表示される。ログイン後にリダイレクト)
- * /signin ログインページ
- */
 const App: VFC = () => {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/public" element={<PublicPage />} />
-        <Route
-          path="/private"
-          element={
-            <RequireAuth>
-              <PrivatePage />
-            </RequireAuth>
-          }
-        />
-        
-        {/* <Route path="/signin" element={<LoginPage moveTo="/" />} /> */}
-        <Route path="/signin" element={<SignIn />} />
-      </Routes>
-      <div>
-        <NavLink to="/">Home</NavLink>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/public" element={<PublicPage />} />
+          <Route
+            path="/private"
+            element={
+              <RequireAuth>
+                <PrivatePage />
+              </RequireAuth>
+            }
+          />
+          <Route path="/signin" element={<SignIn />} />
+        </Routes>
+        <div>
+          <NavLink to="/">Home</NavLink>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
