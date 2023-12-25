e7fa0855ed63a254eb6769fac3378d559d02b871
mar-gitacont <ichikawa.contact@gmail.com>
2023-12-26 01:55:48 +0900

    typescriptバージョン修正

--- src/utils/init-firebase.ts
+++ src/utils/init-firebase.ts
@@ -2,14 +2,25 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, browserSessionPersistence } from 'firebase/auth';
 
 // FirebaseApp初期化
+// initializeApp({
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+//   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+// });
+
 initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC4OJY74U4sMMA5l_DeawKDQM4RiSXYYWA",
+  authDomain:"dog-note.firebaseapp.com",
+  projectId: "dog-note",
+  storageBucket: "dog-note.appspot.com",
+  messagingSenderId: "359892341915",
+  appId: "1:359892341915:web:2c0b5a3e93cbf86f6c623e",
 });
+
+
 
 console.log(process.env.REACT_APP_AUTH_PERSISIT);
 const persisit = process.env.REACT_APP_AUTH_PERSISIT ?? '0';
